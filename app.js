ca9616a05305df0e3a67fb2129b915e42bc0e685
milenabsilva10 <milena.b.silva10@aluno.senai.br>
2026-06-15 08:48:43 -0300

    update

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(errorMiddleware);
 async function inicializarBanco() {
   const client = new Client({
     connectionString:
-      "postgres://admin:vLdQ7edWKnLhO0s5IOJpYsa61UVOhQtt@dpg-d8nksburnols73dsn8ig-a.ohio-postgres.render.com/products_db_vaav?ssl=true",
+      "postgresql://admin:T2z8poA56VzJqO1nPlm3Obvyof1M62zG@dpg-d8ntotgk1i2s73ahp440-a/db_products_czzj?ssl=true",
   });
 
   try {
